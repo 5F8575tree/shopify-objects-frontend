6873023801e7e34935d1f9150218f8e1c8a55458
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2024-01-24 12:21:18 +0900

    expanded child data list and light styling

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import styles from '../app/styles/products.module.css';
 
 function Products({ products }) {
+  const [expandedProductId, setExpandedProductId] = useState([]);
+
+  const toggleDetails = (productId) => {
+    setExpandedProductId(expandedProductId === productId ? null : productId);
+  }
+
   return (
     <div className={styles.products}>
       {products.map(product => (
         <div className={styles.product} key={product.id}>
           <h3>{product.handle}</h3>
           <div className={styles.databox}>
-            <button>Direct Data</button>
+            <button onClick={() => toggleDetails(product.id)}>Direct Data</button>
             <button>Child Data</button>
           </div>
+          {expandedProductId === product.id && (
+            <div className={styles.productDetails}>
+              <p>Title <span>{product.title}</span></p>
+              <p>Handle <span>{product.handle}</span></p>
+              <p>Id <span>{product.id}</span></p>
+              <p>Type <span>{product.product_type}</span></p>
+              <a href={product.image.src}>Image <span>{product.image.id}</span></a>
+            </div>
+          )}
         </div>
       ))}
     </div>

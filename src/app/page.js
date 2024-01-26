40b415a129870886ce0a293366ef429095d5d700
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2024-01-26 13:53:58 +0900

    style header section up until the user clicks view-more btn

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,11 @@ export default function Home() {
   const [collections, setCollections] = useState([]);
   const [products, setProducts] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [showCards, setShowCards] = useState(false);
+
+  const handleToggleCards = () => {
+    setShowCards(!showCards);
+  }
 
   useEffect(() => {
     getShopifyCollections()
@@ -74,66 +79,73 @@ export default function Home() {
             <p>{section.content}</p>
           </div>
         ))}
-        <ul className={styles.cards}>
-          <Card title="Collections">
-            <li>categorization</li>
-            <li>custom colelctions</li>
-            <li>smart collections</li>
-            <li>storefront organization</li>
-            <li>promotional campaigns</li>
-          </Card>
-          <Card title="Products">
-            <li>inventory management</li>
-            <li>variants and options</li>
-            <li>stock management</li>
-            <li>product organization</li>
-            <li>SEO optimization</li>
-          </Card>
-          <Card title="Orders">
-            <li>order processing</li>
-            <li>customer communication</li>
-            <li>order adjustments</li>
-            <li>payment management</li>
-            <li>analytics and reporting</li>
-          </Card>
-          <Card title="Customers">
-            <li>customer profiles</li>
-            <li>segmentation</li>
-            <li>personalization</li>
-            <li>customer service</li>
-            <li>loyalty programs</li>
-          </Card>
-          <Card title="Inventory">
-            <li>inventory tracking</li>
-            <li>stock adjustments</li>
-            <li>low stock alerts</li>
-            <li>inventory forecasting</li>
-          </Card>
-          <Card title="Discounts">
-            <li>discount code creation</li>
-            <li>discount rules</li>
-            <li>time-limited offers</li>
-            <li>performance tracking</li>
-          </Card>
-          <Card title="Shipping and Fulfillment">
-            <li>shipping rate & methods</li>
-            <li>order fulfillment</li>
-            <li>carrier integration</li>
-            <li>return management</li>
-          </Card>
-          <Card title="Analytics and Reports">
-            <li>sales reports</li>
-            <li>customer insights</li>
-            <li>product performance</li>
-            <li>traffic analysis</li>
-          </Card>
-          <Card title="Webhooks" />
-          <Card title="Metafields" />
-          <Card title="Pages and Blogs" />
-          <Card title="Themes and Assets" />
-          <Card title="Script Tags and App Embeds" />
-          <Card title="User and Permissions" />
-        </ul>
+        <div className={styles.showCardsWrapper}>
+          <button className={styles.showCardsBtn} onClick={handleToggleCards}>
+            {showCards ? "Hide Details" : "View More"}
+          </button>
+        </div>
+        {showCards && (
+          <ul className={styles.cards}>
+            <Card title="Collections">
+              <li>categorization</li>
+              <li>custom colelctions</li>
+              <li>smart collections</li>
+              <li>storefront organization</li>
+              <li>promotional campaigns</li>
+            </Card>
+            <Card title="Products">
+              <li>inventory management</li>
+              <li>variants and options</li>
+              <li>stock management</li>
+              <li>product organization</li>
+              <li>SEO optimization</li>
+            </Card>
+            <Card title="Orders">
+              <li>order processing</li>
+              <li>customer communication</li>
+              <li>order adjustments</li>
+              <li>payment management</li>
+              <li>analytics and reporting</li>
+            </Card>
+            <Card title="Customers">
+              <li>customer profiles</li>
+              <li>segmentation</li>
+              <li>personalization</li>
+              <li>customer service</li>
+              <li>loyalty programs</li>
+            </Card>
+            <Card title="Inventory">
+              <li>inventory tracking</li>
+              <li>stock adjustments</li>
+              <li>low stock alerts</li>
+              <li>inventory forecasting</li>
+            </Card>
+            <Card title="Discounts">
+              <li>discount code creation</li>
+              <li>discount rules</li>
+              <li>time-limited offers</li>
+              <li>performance tracking</li>
+            </Card>
+            <Card title="Shipping and Fulfillment">
+              <li>shipping rate & methods</li>
+              <li>order fulfillment</li>
+              <li>carrier integration</li>
+              <li>return management</li>
+            </Card>
+            <Card title="Analytics and Reports">
+              <li>sales reports</li>
+              <li>customer insights</li>
+              <li>product performance</li>
+              <li>traffic analysis</li>
+            </Card>
+            <Card title="Webhooks" />
+            <Card title="Metafields" />
+            <Card title="Pages and Blogs" />
+            <Card title="Themes and Assets" />
+            <Card title="Script Tags and App Embeds" />
+            <Card title="User and Permissions" />
+          </ul>
+        )}
       </div>
       <div className={styles.description}>
         <div className={styles.navigation}>
